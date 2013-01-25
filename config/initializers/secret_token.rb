8c5622933e7f0a3050c4938ea129d966def8dfac
Thorbjørn Hermansen <thhermansen@gmail.com>
2013-01-25 19:07:42 +0100

    Tomt Rails prosjekt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '0aa22ce2204b252aeddc4fd1be6693f9c506bfe3274d85ddf46679c7d200959e3a79926856221132b00e10fcc344d66f95bad23fd7c75d42032ec45dbb964ed7'
