eac2cc0e586248e27c43d7e62e5bd953ff5d76ec
Thorbjørn Hermansen <thhermansen@gmail.com>
2013-01-25 19:10:37 +0100

    Set asset debug to false - may lead to loading errors.
    Ember js files may be loaded in wrong order.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,6 @@ Todo::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
   config.ember.variant = :development
 end
